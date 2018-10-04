f45a90fec75fd2be4daf1285529902148098ec01
User <user>
2018-10-05 02:27:04 +1000

    add alert with destination filepath

--- android/src/main/java/io/wifi/p2p/FileServerAsyncTask.java
+++ android/src/main/java/io/wifi/p2p/FileServerAsyncTask.java
@@ -1,9 +1,12 @@
 package io.wifi.p2p;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
 
@@ -37,9 +40,10 @@ public class FileServerAsyncTask extends AsyncTask<Void, Void, String> {
             System.out.println("Server: Socket opened");
             Socket client = serverSocket.accept();
             System.out.println("Server: connection done");
+            String downloadPath = "Download" + "/wifip2pboxroom-" + System.currentTimeMillis()
+                    + ".jpg";
             final File f = new File(Environment.getExternalStorageDirectory() + "/"
-                    + "Download" + "/wifip2pboxroom-" + System.currentTimeMillis()
-                    + ".jpg");
+                    + downloadPath);
             File dirs = new File(f.getParent());
             if (!dirs.exists())
                 dirs.mkdirs();
@@ -47,7 +51,23 @@ public class FileServerAsyncTask extends AsyncTask<Void, Void, String> {
             System.out.println("server: copying files " + f.toString());
             InputStream inputstream = client.getInputStream();
             copyFile(inputstream, new FileOutputStream(f));
-            serverSocket.close();
+//            serverSocket.close();
+            AlertDialog.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                builder = new AlertDialog.Builder(context);
+            }
+            builder.setTitle("Download Success")
+                    .setMessage("File downloaded to " + downloadPath + ".")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // continue with delete
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
             return f.getAbsolutePath();
         } catch (IOException e) {
             System.err.println(e.getMessage());
